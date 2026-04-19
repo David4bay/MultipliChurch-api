22253e98313d36b718222dba956408fcc831a836
David Bayode <david4bay@gmail.com>
2026-04-19 06:28:24 +0100

    commented out versioned route

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const appRouter = require("./app")
 const PORT = process.env.PORT || 3000
 
 app.use(require("express").json())
-app.use("/api/v1", appRouter)
+// app.use("/api/v1", appRouter)
+app.use(appRouter)
 app.use(function(req, res) {
     return res.status(404).json({ message: "Route not found" })
 })
