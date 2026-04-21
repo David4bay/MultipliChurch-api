18d894e6a2bd525f0627988a6ddc8929ad566686
David Bayode <david4bay@gmail.com>
2026-04-21 16:22:59 +0100

    removed serverless function

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
-
-// Vercel requires exporting the app
-module.exports = app;
